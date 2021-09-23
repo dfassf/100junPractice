c1a85f9ad5e42d05c1f498be2010c3b6dcb6ade9
dfassf <dfassf@gmail.com>
2021-09-23 17:36:39 +0900

    lvl8

--- lvl8/2869.js
+++ lvl8/2869.js
@@ -1,21 +1,22 @@
 
-let input = '2 1 5'
-let a = input.split(' ')[0]
-let b = input.split(' ')[1]
-let v = input.split(' ')[2]
+let input = '100 99 1000000000'
+let a = Number(input.split(' ')[0])
+let b = Number(input.split(' ')[1])
+let v = Number(input.split(' ')[2])
 
-// per day = n
-// (a-b)*n=v
+// 최초에 풀었던 방식, 시간초과 뜸
 
-let n = 1
-let height = 0
-while(true){
-    height += Number(a)
-    height -= Number(b)
-    
-    n ++
+// let n = 0
+// let result = 0
+// while(true){
 
-    if(height>=Number(v)){break}
-}
+//     n++
+//     result += a
+//     if(result>=v){break}
+//     result -= b
 
-console.log(n)
+// }
+// console.log(n)
+
+console.log(Math.ceil((v-b)/(a-b)))
+
