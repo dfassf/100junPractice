1d83886701716cccb00d271c11ad6608d3ef7c67
dfassf <dfassf@gmail.com>
2021-10-11 20:53:09 +0900

    lvl8

--- lvl8/10250.js
+++ lvl8/10250.js
@@ -1,21 +1,20 @@
-// const input = require('fs').readFileSync('/dev/stdin').toString().split('\n')
-const input = `2
-6 12 10
-30 50 72`.split('\n')
-const howMany = Number(input.shift())
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const howMany = parseInt(input.shift()); 
 
-for(let i = 0; i<howMany; i++){
-    let height = Number(input[i].split(' ').shift())
-    input[i].split(' ').shift()
-    let number = Number(input[i].split(' ').shift())
-    let cnt = 1
-
-    while(number>height){
-        cnt++
-        number -= height
-    }
-
-    if(cnt<10){
-        console.log(`${number}0${cnt}`)
-    } else{ console.log(`${number}${cnt}`)}
+for (let i = 0; i < howMany; i++) {
+  const arr = input[i].split(' ');
+  let height = parseInt(arr.shift());
+  arr.shift(); 
+  let number = parseInt(arr.shift()); 
+  let cnt = 1; 
+  
+  while (number > height) {
+    cnt++;
+    number = number - height;
+  } 
+  if (cnt < 10) {
+    console.log(`${number}0${cnt}`);
+  } else {
+    console.log(`${number}${cnt}`);
+  }
 }
